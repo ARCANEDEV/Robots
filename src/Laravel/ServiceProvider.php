5dcf69eab47bcc90358a3d6630182e47f7466d42
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-05-14 15:44:48 +0100

    Add Laravel support (v4.2)

---
+++ src/Laravel/ServiceProvider.php
@@ -0,0 +1,53 @@
+<?php namespace Arcanedev\Robots\Laravel;
+
+use Arcanedev\Robots\Robots;
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('arcanedev.robots', function () {
+            return new Robots;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['arcanedev.robots'];
+    }
+}
